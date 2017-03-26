e4b67145b177e0d1f013c4762bc278e001d9e7d1
Henrique <rtrans.web@gmail.com>
2017-03-26 16:11:49 +0200

    Create createNodeAndText.js

---
+++ createNodeAndText.js
@@ -0,0 +1,80 @@
+    /* **************************************
+     * ************        
+     *                            
+     * Create a NodeText
+     *                            
+     * ************        
+     ************************************** */
+    
+    function createText(text) {
+
+        return document.createTextNode(text);
+
+    }
+    
+    
+    
+    
+    
+    
+    
+    /* **************************************
+     * ************        
+     *                            
+     * Create a NodeElementHtml
+     *                            
+     * ************        
+     ************************************** */
+   
+    function create(tag, attributesOrText) {
+
+        /* functions */
+        function createText(text) {
+
+            return document.createTextNode(text);
+
+        }
+
+        /* create element */
+        var element = document.createElement(tag);
+
+        /* **************************************
+         * ************
+         *
+         * create attributes
+         *
+         * ************
+         ************************************** */
+
+        if (typeof attributesOrText !== 'undefined') {
+
+            /* id */
+            if (typeof attributesOrText['id'] !== 'undefined') {
+                element.id = attributesOrText['id'];
+            }
+
+            /* href */
+            if (typeof attributesOrText['href'] !== 'undefined') {
+                element.href = attributesOrText['href'];
+            }
+
+            /* class */
+            if (typeof attributesOrText['class'] !== 'undefined') {
+                element.classList = attributesOrText['class'];
+            }
+
+            /* title */
+            if (typeof attributesOrText['title'] !== 'undefined') {
+                element.title = attributesOrText['title'];
+            }
+
+            /* TEXTE */
+            if (typeof attributesOrText['text'] !== 'undefined') {
+                element.appendChild(createText(attributesOrText['text']));
+            }
+
+        }
+        
+        return element;
+
+    }
